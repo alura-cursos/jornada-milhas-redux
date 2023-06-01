26461e1ba217d40fd33e13b86b593f23fc43926f
Luiz Fernando <lfrprazeres@gmail.com>
2023-06-01 14:12:42 -0300

    3.2 - configurando o flipper

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Middleware } from '@reduxjs/toolkit';
 import usuario from './reducers/usuario';
+import createDebugger from 'redux-flipper';
+
+const middlewares: Middleware[] = [];
+
+if (__DEV__) {
+  middlewares.push(createDebugger());
+}
 
 const store = configureStore({
   reducer: {
     usuario
-  }
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().prepend(...middlewares)
+  ,
 });
 
 export default store;
