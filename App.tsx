5200267e3903b5f8e2519ca59e5594aa87480d94
Luiz Fernando <lfrprazeres@gmail.com>
2023-06-01 12:11:37 -0300

    2.1 - criando o store

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { Provider as RNPProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { Provider } from 'react-redux';
 import Routes from 'src/routes';
 import theme from 'src/config/theme';
 import { SnackbarProvider } from 'src/contexts/Snackbar';
+import store from 'src/store';
 
 export default function App() {
   return (
+    <Provider store={store}>
       <RNPProvider theme={theme}>
         <SnackbarProvider>
           <NavigationContainer>
@@ -17,5 +19,6 @@ export default function App() {
           </NavigationContainer>
         </SnackbarProvider>
       </RNPProvider>
+    </Provider>
   );
 }
